2367117b1bc93c40f7d72f158145b7feaa98ad79
Mathias Bak <bufas7@gmail.com>
2013-02-27 12:36:31 +0100

    Take input as a single string and output values of variables.

--- src/Simplex.java
+++ src/Simplex.java
@@ -10,15 +10,53 @@ public class Simplex {
 
     private Fraction[][] program;
     private List<Fraction[][]> iterations;
+    private int[] basis;
 
-    public Simplex() throws IOException {
+    public Simplex(String programString, String templateFile, String substitutionString) throws IOException {
+        program = stringToProgram(programString);
+
+        System.out.println("\nLinear Program to solve:");
+        printProgram();
+
         iterations = new ArrayList<Fraction[][]>();
-        fetchInput();
+        //fetchInput();
         solve();
         printSolution();
+        outputLatex();
     }
 
-    public void fetchInput() throws IOException {
+    private Fraction[][] stringToProgram(String program) {
+        String[] input = program.split(" ");
+
+        // Fetch number of variables and number of constraints
+        int vs = Integer.parseInt(input[0]);
+        int cs = Integer.parseInt(input[1]);
+        Fraction[][] result = new Fraction[cs+1][vs+cs+1];
+
+        // Define the basis
+        basis = new int[cs];
+        for (int i = 0; i < cs; i++) {
+            basis[i] = vs+i;
+        }
+
+        // Build the program matrix
+        for (int i = 0; i < cs+1; i++) {
+            int start = 2 + (i * (vs + 1));
+
+            for (int j = 0; j < vs; j++)
+                result[i][j] = new Fraction(Integer.parseInt(input[start+j]));
+            // Insert the slack variables into the program matrix
+            for (int j = 0; j < cs; j++)
+                result[i][vs+j] = new Fraction(j==i ? 1 : 0);
+            // Insert last value, but not to the objective function
+            if (i != cs) result[i][vs+cs] = new Fraction(Integer.parseInt(input[start + vs]));
+            else result[i][vs+cs] = new Fraction(0);
+        }
+
+        return result;
+    }
+
+    private void fetchInput() throws IOException {
         BufferedReader bufferRead = new BufferedReader(new InputStreamReader(System.in));
         System.out.print("Input number of variables:   ");
         int variables   = Integer.parseInt(bufferRead.readLine());
@@ -48,10 +86,14 @@ public class Simplex {
         printProgram();
     }
 
-    public void solve() {
+    private void solve() {
         int pivotCol;
         while ((pivotCol = getpivotCol()) != -1) {
             int pivotRow = getpivotRow(pivotCol);
+
+            // Update the basis
+            basis[pivotRow] = pivotCol;
+
             if (pivotRow == -1) {
                 System.out.println("THE PROBLEM IS UNBOUNDED");
                 System.exit(1);
@@ -77,13 +119,29 @@ public class Simplex {
                 }
             }
 
+            iterations.add(program);
             System.out.println("\nThe pivot row position was: (" + pivotCol + "," + pivotRow + ") = " + pivotNumber);
             printProgram();
         }
     }
 
+    private void outputLatex() {
+
+    }
+
     private void printSolution() {
         System.out.println("\nTHE SOLUTION IS z = " + program[program.length-1][program[0].length-1].negate());
+
+        // Initialize array
+        Fraction[] values = new Fraction[program[0].length-1];
+        for (int i = 0; i < program[0].length-1; i++) values[i] = Fraction.ZERO;
+
+        for (int i = 0; i < basis.length; i++) {
+            int var = basis[i];
+            values[var] = program[i][program[0].length-1];
+        }
+
+        for (int i = 0; i < program[0].length-1; i++) System.out.println("\tx_"+(i+1)+" = " + values[i]);
     }
 
     private int getpivotCol() {
@@ -100,11 +158,9 @@ public class Simplex {
     }
     
     private int getpivotRow(int pivotCol) {
-        System.out.println("Getting pivotRow for column " + pivotCol);
         Fraction smallestRatio = Fraction.MINUS_ONE;
         int pivotRow = -1;
         for (int i = 0; i < program.length-1; i++) {
-            System.out.println("Trying row " + i);
             Fraction value = program[i][pivotCol];
             if (value.equals(Fraction.ZERO)) continue; // Never go full retard!
             Fraction ratio = program[i][program[i].length-1].divide(value);
@@ -127,9 +183,8 @@ public class Simplex {
     }
 
     public static void main(String[] args) throws IOException {
-        Simplex s = new Simplex();
-        s.solve();
-
+        String test = "3 3 2 3 1 5 4 1 2 11 3 4 2 8 5 4 3";
+        Simplex s = new Simplex(test, "template.tex", "% Insert Simplex Solution");
     }
 
 }
